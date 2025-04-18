477cbbd35454fe79529d0ec1a4ea04cbe21bcf0c
razzlematazzle <25349723+razzlematazzle@users.noreply.github.com>
2025-04-18 16:19:36 +0000

    Making TransactionClient a component

--- roundup/src/main/java/sb/roundup/roundup/Client/TransactionClient.java
+++ roundup/src/main/java/sb/roundup/roundup/Client/TransactionClient.java
@@ -2,11 +2,14 @@ package sb.roundup.roundup.Client;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import sb.roundup.roundup.Model.Transaction;
 
 /***
  * Class for retreiving transactions from transaction endpoin
  */
+@Component
 public class TransactionClient {
 
 
